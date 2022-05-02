0207dfcfc45b2dc2a50a602be79395678beb510a
Igor Pedrosa <ilap1036@gmail.com>
2022-05-02 16:13:23 -0300

    fauna connection | api route signin callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,48 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from './../../../services/fauna';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,  
     }),
-    // ...add more providers here
   ],
+  // secret: process.env.SECRET_KEY,
+  callbacks: {
+    async signIn({user, account, profile, email, credentials}) {
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: user.email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+
+    },
+  }
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
-import styles from './home.module.scss';
-import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
