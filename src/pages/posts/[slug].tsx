15bc1608d3688c67f34967efc8e816d9da4832a2
IgorLAP <ilap1036@gmail.com>
2022-05-09 10:37:43 -0300

    feat: dynamic post component

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,65 @@
+import { asHTML, asText } from '@prismicio/helpers';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
+import Head from 'next/head';
+
+import { createClient } from '../../services/prismicio';
+import styles from './post.module.scss';
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post ({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ignews - criticas</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.article}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div 
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: post.content }} 
+          />
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = getSession({ req })
+
+  // if (!session) {
+
+  // }
+
+  const { slug } = params;
+  const prismic = createClient(req)
+  const response = await prismic.getByUID('publication', slug as string, {})
+  
+  const post = {
+    slug,
+    title: asText(response.data.title),
+    content: asHTML(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
