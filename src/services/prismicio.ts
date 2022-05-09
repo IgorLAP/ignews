984577006d46bb7daf1e41f959007caf1524e633
IgorLAP <ilap1036@gmail.com>
2022-05-09 10:34:51 -0300

    refactor: option of a req parameter on createClient prismic method

--- src/services/prismicio.ts
+++ src/services/prismicio.ts
@@ -5,8 +5,8 @@ import sm from './../../sm.json';
 export const endpoint = sm.apiEndpoint
 export const repositoryName = prismic.getRepositoryName(endpoint)
 
-export function createClient() {
-  const client = prismic.createClient(endpoint)
+export function createClient(req?: unknown) {
+  const client = prismic.createClient(endpoint, req)
 
   return client
 }
