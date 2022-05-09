a2758b5d1de8b2ec1355c694554c2a49562eb10f
Igor Pedrosa <ilap1036@gmail.com>
2022-05-09 16:16:15 -0300

    feat: preview pages

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,7 +38,7 @@ export default function Post ({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req })
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,86 @@
+import { asHTML, asText } from '@prismicio/helpers';
+import { GetStaticProps } from 'next';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import { createClient } from '../../../services/prismicio';
+import styles from './../post.module.scss';
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function PostPreview ({ post }: PostPreviewProps) {
+  const { data } = useSession();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    console.log(data)
+    if(data?.activeSubscription) {
+      console.log(data?.activeSubsciption)
+      push(`/posts/${post.slug}`)
+    }
+  }, [data])
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ignews - criticas</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.article}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div 
+            className={`${styles.content} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }} 
+          />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href='/'>
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = createClient()
+  const response = await prismic.getByUID('publication', slug as string, {})
+  
+  const post = {
+    slug,
+    title: asText(response.data.title),
+    content: response.data.content[0].text.split('\n')[0],
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
